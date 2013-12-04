a97dd6047c1a4fd8c0a7485c56f145c9637c7183
George Vas <georgevassiliadis@hotmail.com>
2013-12-04 21:15:17 +0100

    key calculation function completed (not tested)

--- CryptographyApp/Des.cs
+++ CryptographyApp/Des.cs
@@ -28,12 +28,16 @@ namespace CryptographyApp
             }
         }
 
-        private char keyPermutation(string initSeq)
+        private char[] keyPermutation<char[],char[]>(string initSeq)
         {
+            char[] Key1 = new char[12];
+            char[] Key2 = new char[12];
             char[] pc1Seq = new char[14];
             char[] leftPart = new char[7];
             char[] rightPart = new char[7];
-            int[] permOrdr1 = new int[14] { 11, 15, 4, 13, 7, 9, 3, 2, 5, 14, 6, 10, 12, 1 };
+            char[] proKey = new char[14];
+            int[] permOrdr1 = new int[14] { 10, 14, 3, 12, 6, 8, 2, 1, 4, 13, 5, 9, 11, 0 };
+            int[] permOrdr2 = new int[12] { 5, 10, 3, 7, 12, 2, 11, 4, 0, 9, 1, 8 };
 
             //performs the first permutation of the key.
             for (int i = 0; i < 14; i++)
@@ -46,6 +50,40 @@ namespace CryptographyApp
             //assign the values in D
             for (int i = 0; i < 7; i++)
                 rightPart[i] = pc1Seq[i + 7];
+
+            rightPart = LeftShift(rightPart);
+            proKey = LeftShift(leftPart);
+
+            for (int i = 0; i < 7; i++)
+                proKey[i + 7] = rightPart[i];
+
+            for (int i = 0; i < 12; i++)
+                Key1[i] = proKey.ElementAt<char>(permOrdr2[i]);
+
+            rightPart = LeftShift(rightPart);
+            proKey = LeftShift(LeftShift(leftPart));
+
+            for (int i = 0; i < 7; i++)
+                proKey[i + 7] = rightPart[i];
+
+            for (int i = 0; i < 12; i++)
+                Key2[i] = proKey.ElementAt<char>(permOrdr2[i]);
+        }
+
+        private char[] LeftShift(char[] bits)
+        {
+            char a = bits[0];
+            char b = bits[1];
+            for (int i = 0, j = bits.Count(); i < j; i++)
+            {
+                if (i < j - 2)
+                    bits[i] = bits[i + 2];
+                else if (i == j - 2)
+                    bits[i] = a;
+                else if (i == j - 1)
+                    bits[i] = b;
+            }
+            return bits;
         }
     }
 }
